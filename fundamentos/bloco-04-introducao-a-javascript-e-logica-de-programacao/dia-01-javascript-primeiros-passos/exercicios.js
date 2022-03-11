04c19a47a1354b69d56747147110bfce8fcb8291
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-03-11 19:01:34 -0300

    inclui os exercicios 2 e 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
@@ -9,3 +9,30 @@ console.log(a * b);
 console.log(a / b);
 console.log(a % b);
 
+//Exercício 2
+
+const numero1 = 1862;
+const numero2 = 9272;
+
+if (numero1 > numero2) {
+    console.log("o primeiro número é maior");
+}
+else {
+    console.log("o segundo número é maior");
+}
+
+//Exercício 3
+
+const primeiro = 10;
+const segundo = 40;
+const terceiro = 50;
+
+if (primeiro > segundo && primeiro > terceiro) {
+    console.log("o primeiro é maior");
+}
+else if (segundo > primeiro && segundo > terceiro) {
+    console.log("o segundo é maior");
+}
+else {
+    console.log("o terceiro é maior")
+}
