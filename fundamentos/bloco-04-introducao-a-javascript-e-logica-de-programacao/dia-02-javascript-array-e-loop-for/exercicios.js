16b55e7216a357201cc41e4620d19cac568c2b62
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-03-14 16:05:19 -0300

    cria arquivo da lista de exercicios e inclui os exercicios 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -0,0 +1,16 @@
+//Exercício 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i]);
+}
+
+//Exercício 2
+
+let sum = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    sum += numbers[index]
+}
+console.log(sum);
