c68771845a86dc2a2971320b63d6f556518a8afc
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-03-14 19:26:10 -0300

    inclui exercicio 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -2,7 +2,7 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (i = 0; i < numbers.length; i += 1) {
+/* for (i = 0; i < numbers.length; i += 1) {
     console.log(numbers[i]);
 }
 
@@ -33,4 +33,16 @@ if (average > 20) {
 }
 else {
     console.log('valor menor ou igual a 20');
+} */
+
+//Exercício 5
+
+let maiorNumero = numbers[0];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index];
+    }
 }
+
+console.log(maiorNumero);
