a4afe6b1b84a1c27e9edc5d5a643ecc8572597aa
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-03-14 12:33:16 -0300

    inclui exercicio de fixaçao de laço for of

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/fixacao.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/fixacao.js
@@ -35,3 +35,13 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (index = 0; index < groceryList.length; index += 1) {
     console.log(groceryList[index]);
 }
+
+//For/of
+
+//Exercício 1
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let pessoas of names) {
+    console.log(pessoas);
+}
