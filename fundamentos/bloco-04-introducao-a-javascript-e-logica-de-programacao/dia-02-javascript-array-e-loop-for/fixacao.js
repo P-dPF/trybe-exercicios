54dfc262bc56965acc7883898c60461c2b23771f
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-03-14 11:38:06 -0300

    cria arquivo e acrescenta exercicios de fixaçao de arrays

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/fixacao.js
@@ -0,0 +1,25 @@
+//Exercício de Fixação - Dia 4.2
+
+//Exercício 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//Exercício 2
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu1.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//Exercício 3
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+console.log(menu2);
+
+menu2.push('Contato');
+
+console.log(menu2);
