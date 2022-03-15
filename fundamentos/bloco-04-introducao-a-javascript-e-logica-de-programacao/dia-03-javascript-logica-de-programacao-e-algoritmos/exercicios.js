63ef858cc4bba2347e54038458ee1830f5e1b32b
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-03-15 00:15:27 -0300

    inclui exercicio 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -9,3 +9,16 @@ for (i = 10; i >= 1; i -= 1) {
 }
 
 console.log(fatorial);
+
+//Exercício 2
+
+let word = 'tryber';
+
+let novaPalavra = ''
+
+for (i = word.length -1; i >= 0; i -= 1) {
+    novaPalavra += word[i];
+}
+
+console.log(novaPalavra);
+
