37bd019c0b79c698c8e975e5ed8f0e017086e0c9
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-03-31 00:58:35 -0300

    inclui exercicio 1 do bonus - habilita butao de file somente com o checkbox de agree ativado

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/form.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/form.js
@@ -3,3 +3,16 @@ let submitBtn = document.getElementById('submit');
 submitBtn.addEventListener('click', function(event) {
     event.preventDefault();
 });
+
+let picUploadBtn = document.getElementById('trip-photos');
+
+let agreeBtn = document.getElementById('agree');
+
+agreeBtn.addEventListener('click', function(event) {
+    if (event.target.checked === true) {
+        picUploadBtn.disabled = false;
+    }
+    else {
+        picUploadBtn.disabled = true;
+    }
+});
