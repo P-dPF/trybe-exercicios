a348dcaa71ad353915fc1fbec50a1eab3427e57d
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-04-18 17:26:06 -0300

    incluirascinho do  exercício 3

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicios.js
@@ -25,7 +25,29 @@ const checkResult = (firstNumber, secondNumber) => firstNumber === secondNumber;
 
 const prize = (betNumber, callback) => {
   answerNumber = Math.floor(Math.random() * 5 + 1);
-  return callback(betNumber, answerNumber) ? 'Parabéns você ganhou': 'Tente novamente';
+  return callback(betNumber, answerNumber) ? 'Parabéns você ganhou' : 'Tente novamente';
 }
 
 console.log(prize(1, checkResult));
+
+// Exercício 3
+
+const answerCheck = (firstArray, secondArray) => {
+  scoreCount = 0;
+  for (let i = 0; i < firstArray.length; i += 1) {
+    for (let i2 = 0; i2 < secondArray.length; i += 1) {
+      if (firstArray[i] === secondArray[i2]) {
+        scoreCount += 1;
+      } else if (firstArray !== secondArray && secondArray !== 'N.A') {
+        scoreCount -= 0.5;
+      }
+    }
+  }
+  return scoreCount;
+}
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const evaluation = (rightAnswers, studentAnswers, callback) => {}
