d2125b9f492ff73aaae1d1873033853075298f18
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-04-19 19:35:11 -0300

    fixação find exercicio 2

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixacao.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixacao.js
@@ -20,3 +20,11 @@ const numbers = [19, 21, 30, 3, 45, 22, 15];
 const findDivisibleBy3And5 = () => numbers.find((number) => number % 3 === 0  && number % 5 === 0);
 
 console.log(findDivisibleBy3And5())
+
+// EXERCÍCIO 2
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => names.find((name) => name.length === 5);
+
+console.log(findNameWithFiveLetters());
