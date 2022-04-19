dccebdb5edb46500d70e6884ffddda03dcc6c7d5
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-04-19 19:32:57 -0300

    fixação find exercicio 1

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixacao.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixacao.js
@@ -12,3 +12,11 @@ const showEmailList = (email) => {
 };
 
 emailListInData.forEach(showEmailList);
+
+// FIND - EXERCÍCIO 1
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => numbers.find((number) => number % 3 === 0  && number % 5 === 0);
+
+console.log(findDivisibleBy3And5())
