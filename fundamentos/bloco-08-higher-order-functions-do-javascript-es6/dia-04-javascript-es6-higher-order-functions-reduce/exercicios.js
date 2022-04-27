d13564079e56f6994f6f94cbd153e5fd0c83f907
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-04-27 17:05:29 -0300

    lista rascunho exercicio 6

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicios.js
@@ -110,3 +110,35 @@ function longestNamedBook() {
 }
 
 console.log(longestNamedBook());
+
+// EXERCÍCIO 5
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  
+}
+
+console.log(containsA());
+
+// EXERCÍCIO 6
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  return students.map();
+}
+
+console.log(studentAverage());
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
