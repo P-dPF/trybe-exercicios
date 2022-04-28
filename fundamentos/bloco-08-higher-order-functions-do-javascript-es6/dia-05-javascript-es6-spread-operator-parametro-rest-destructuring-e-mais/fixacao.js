309141256ebd372a2bf02cd0a408ea7370d8af4f
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-04-28 17:08:10 -0300

    inclui fixação de object destructuring

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao.js
@@ -1,3 +1,5 @@
+// SPREAD OPERATOR
+
 // Faça uma lista com as suas frutas favoritas
 const specialFruit = ['Banana', 'Kiwi', 'Mirtilo'];
 
@@ -10,3 +12,23 @@ const fruitSalad = (fruit, additional) => {
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));
+
+// OBJECT DESTRUCTURING
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const generalInfo = { ...user, ... jobInfos};
+
+const { name, age, nationality, profession, squad, squadInitials } = generalInfo;
+
+console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm Brazilian. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
