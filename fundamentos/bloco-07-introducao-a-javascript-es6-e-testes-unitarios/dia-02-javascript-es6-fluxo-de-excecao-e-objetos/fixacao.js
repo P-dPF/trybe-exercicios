58200f7319b28b6aef65891bea963f5b58a5551f
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-03 18:50:09 -0300

    inclui arquivos do dia 7.2 e exercicio de fixação da parte 2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/fixacao.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/fixacao.js
@@ -8,3 +8,30 @@ const addNewProperties = (object, key, value) => {
 }
 
 console.log(addNewProperties({ propriedadeUm: 'valorUm' }, 'chaveTeste', 'valorTeste'));
+
+// PARTE 2 - OBJECT.KEYS
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const showPairs = (student) => {
+  let message = [];
+  for (let property in student) {
+    message.push(`${property}, Nível: ${student[property]}`);
+  }
+  return message
+}
+
+console.log(showPairs(student2));
