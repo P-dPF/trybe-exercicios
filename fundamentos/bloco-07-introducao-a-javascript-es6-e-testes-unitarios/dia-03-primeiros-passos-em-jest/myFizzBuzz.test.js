250360c2166d4bbc34f454ba4823672a72f81cdc
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-03 14:55:32 -0300

    inclui o item 2 do exercício 3

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/myFizzBuzz.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/myFizzBuzz.test.js
@@ -7,4 +7,8 @@ describe('verifica se myFizzBuzz dá o retorno esperado conforme a divisibilidad
   test('verifica se myFizzBuzz retorna a string "fizzbuzz" quando o parâmetro recebido é divisível por 3 e por 5', () => {
     expect(myFizzBuzz(15)).toBe('fizzbuzz');
   });
+  // ITEM 2
+  test('verifica se myFizzBuzz retorna a string "fizz" quando o parâmetro recebido é divisível somente por 3', () => {
+    expect(myFizzBuzz(9)).toBe('fizz');
+  });
 });
