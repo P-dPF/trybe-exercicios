ef6e74620d8fc1ff5285e0aff22eebb13f1052ca
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-03 14:22:16 -0300

    inclui o requisito 3 do exercício 2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/myRemove.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/myRemove.test.js
@@ -2,7 +2,7 @@
 
 const myRemove = require('./myRemove');
 
-describe('verifica se myRemove retorna uma cópia do array recebido como parâmetro sem o item recebido como segundo parâmetro', () => {
+describe('verifica se myRemove retorna uma cópia do array recebido como parâmetro sem o item recebido como segundo parâmetro, caso ele exista np array', () => {
   // ITEM 1
   test('testa se myRemove retorna o array esperado com os parâmetros [1, 2, 3, 4] e 3', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
@@ -10,5 +10,9 @@ describe('verifica se myRemove retorna uma cópia do array recebido como parâme
   // ITEM 2
   test('testa se myRemove não retorna [1, 2, 3, 4] quando recebe os parâmetros [1, 2, 3, 4] e 3', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4], 3);
+  });
+  // ITEM 3
+  test('testa se myRemove retorna o array esperado com os parâmetros [1, 2, 3, 4] e 5', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 });
