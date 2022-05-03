7c3bc6a188771b83317242f0fdbef9225a209cfe
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-03 13:55:38 -0300

    inclui o item 2 do exercicio 1

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -5,7 +5,11 @@
 const sum = require('./sum');
 
 describe('verifica se a função soma retorna a soma dos seus parâmetros', () => {
-  test('sum retorna a soma de 4 com 5', () => {
+  test('sum retorna a soma de 4 com 5 igual a 9', () => {
     expect(sum(4, 5)).toEqual(9);
   });
-})
+  // ITEM 2
+  test('sum retorna a soma de 0 com 0 igual a 0', () => {
+    expect(sum(0, 0)).toEqual(0);
+  });
+});
