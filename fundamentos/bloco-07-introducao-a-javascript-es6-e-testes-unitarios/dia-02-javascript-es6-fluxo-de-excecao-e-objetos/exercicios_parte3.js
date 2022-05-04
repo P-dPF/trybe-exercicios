99027c8b3bec31f427f279cd449548a30be5f50d
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-03 23:46:03 -0300

    exercicio 1 da parte 3

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/exercicios_parte3.js
@@ -0,0 +1,29 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// EXERCÍCIO 1
+
+const addShift = (object, key, value) => {
+  const shift = key;
+  object[shift] = value;
+  return object;
+}
+
+console.log(addShift(lesson2, 'turno', 'noite'));
