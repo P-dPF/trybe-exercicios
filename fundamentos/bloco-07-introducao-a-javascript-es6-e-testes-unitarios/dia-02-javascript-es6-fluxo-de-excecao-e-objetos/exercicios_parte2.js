85b3322181bd7f19d41026d8bbf653ebbbcf2c35
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-03 23:36:33 -0300

    exercicio 2 da parte 2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/exercicios_parte2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/exercicios_parte2.js
@@ -34,20 +34,26 @@ const order = {
   },
 };
 
+console.log(Object.keys(order.order.pizza));
+
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
   const deliveryPerson = order.order.delivery.deliveryPerson;
   const customer = order.name;
   const phoneNumber = order.phoneNumber;
   const address = `${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`;
-  return console.log(`Olá ${deliveryPerson}, entrega para: ${customer}, ${phoneNumber}, ${address}`);
+  return console.log(`Olá ${deliveryPerson}, entrega para: ${customer}, Telefone: ${phoneNumber}, ${address}`);
 }
 
 customerInfo(order);
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  order.name = 'Luiz Silva';
+  const foodOrder = Object.keys(order.order.pizza);
+  const drinks = order['order']['drinks']['coke']['type'];
+  const price = order['payment']['total'];
+  return console.log(`Olá ${order.name}, o total do seu pedido de ${foodOrder[0]}, ${foodOrder[1]} e ${drinks} é R$${price},00`);
 }
 
 orderModifier(order);
