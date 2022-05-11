c5b3b54bf869b16c0a44ecbfae5edf3eeabba0a5
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-10 22:37:47 -0300

    refatora o componente App

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
@@ -22,16 +22,12 @@ function App() {
     }
   };
   return (
-    <>
-      <div className="App">
-        <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
-      </div>
-      <div className="App">
-        <h1> Orders recently created </h1>
-        <Order order={headphone} />
-        <Order order={energyDrink} />
-      </div>
-    </>
+    <div className="App">
+      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+      <h1> Orders recently created </h1>
+      <Order order={headphone} />
+      <Order order={energyDrink} />
+    </div>
   );
 }
 
