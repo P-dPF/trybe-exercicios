6fe0b039acab9cae7071d92254cff4afe92dc7d2
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-10 23:51:36 -0300

    refatora componente pokemon

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Components/Pokemon.jsx
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Components/Pokemon.jsx
@@ -4,13 +4,11 @@ import pokemons from "../data";
 class Pokemon extends React.Component {
   render() {
     return (
-      pokemons.map(pokemon => (
-        <div key={pokemon.id}>
-          <h3>{this.props.pokemon.name}</h3>
-          <p>{this.props.pokemon.type}</p>
-          <p>Average weight{this.props.averageWeight.value}{this.props.averageWeight.value}</p>
-        </div>
-      ))
+      <div>
+        <h3>{this.props.pokemon.name}</h3>
+        <p>{this.props.pokemon.type}</p>
+        <p>Average weight{this.props.averageWeight.value}{this.props.averageWeight.value}</p>
+      </div>
     );
   }
 }
