4f6124aebce14eca666bac0202165354b2989a1f
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-10 22:25:02 -0300

    refatora o prop alternativeText passado para Image dentro de App

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Cute cat staring' />
+        <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
       </div>
       <div className="App">
         <h1> Orders recently created </h1>
