f46925ddf758ae938883b86255f2ec5c10046fc5
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-10 22:53:40 -0300

    inclui fixação de criação de lista de componentes e chaves

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,5 +1,6 @@
 import Image from './Image';
 import Order from './Order';
+import UserProfile from './UserProfile';
 
 function App() {
   const headphone = {
@@ -21,12 +22,29 @@ function App() {
       currency: "dollars"
     }
   };
+
+  const users = [
+    {
+      id: 102,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+    },
+    {
+      id: 77,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+    }
+  ];
+
   return (
     <div className="App">
       <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
       <h1> Orders recently created </h1>
       <Order order={headphone} />
       <Order order={energyDrink} />
+      {users.map(user => <UserProfile key={user.id} user={user} />)}
     </div>
   );
 }
