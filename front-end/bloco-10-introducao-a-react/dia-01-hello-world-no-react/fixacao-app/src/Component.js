c4e523779b2a2a493f5f5ff6acf070ea8aa3d816
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-17 00:42:42 -0300

    exercicio de fixação dia 10-1

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/fixacao-app/src/Component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Pedro de Paula França</h1>
+        <p>Sou estudante da Trybe e estou aprendendo React!</p>
+      </div>
+    )
+  }
+}
+
+export default MyComponent;
