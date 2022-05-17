f8ee9004bdb04ec55695f60cb0bfce81ca4e3722
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-17 01:54:09 -0300

    fixação da seção de lista de componentes e chaves

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Image from './Image'
 import Order from './Order';
+import UserProfile from './UserProfile';
 
 class App extends React.Component {
   render() {
@@ -26,6 +27,21 @@ class App extends React.Component {
       }
     };
 
+    const users = [
+      {
+        id: 102,
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      },
+      {
+        id: 77,
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      }
+    ];
+
     return (
       <>
         <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
@@ -34,6 +50,10 @@ class App extends React.Component {
            {<Order order={ headphone } />}
            {<Order order={ energyDrink } />}
       </div>
+      <div className="App">
+        {users.map(user => <UserProfile key={user.id} user={user} />)}
+      </div>
+    );
       </>
     );
   }
