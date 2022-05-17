b35f63e39fcb2112664bfc84d7d52b0a31f19740
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-17 12:44:08 -0300

    fixação da seção de unindo componentes com estados e eventos

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -5,29 +5,51 @@ class App extends React.Component {
 
   constructor() {
     super()
+    
+    this.state = {
+      cliquesPrimeiroBotao: 0,
+      cliquesSegundoBotao: 0,
+      cliquesTerceiroBotao: 0,
+    }
+
     this.handleClick = this.handleClick.bind(this);
-    this.handleSecondClick = this.handleSecondClick(this);
-    this.handleThirdClick = this.handleThirdClick(this);
+    this.handleSecondClick = this.handleSecondClick.bind(this);
+    this.handleThirdClick = this.handleThirdClick.bind(this);
   }
 
   handleClick() {
     console.log('Botão apertado!');
+    console.log(this);
+    /* this.setState(() => ({ cliquesPrimeiroBotao:1 })) */
+    this.setState((estadoAnterior, _props) => ({
+      cliquesPrimeiroBotao: estadoAnterior.cliquesPrimeiroBotao + 1
+    }))
   }
   
   handleSecondClick() {
     console.log('Falei para não apertar!');
+    console.log(this);
+    /* this.setState(() => ({ cliquesSegundoBotao: 1 })) */
+    this.setState((estadoAnterior, _props) => ({
+      cliquesSegundoBotao: estadoAnterior.cliquesSegundoBotao + 1
+    }))
   }
   
   handleThirdClick() {
     console.log('Pare de desobedecer!');
+    console.log(this);
+    /* this.setState(() => ({ cliquesTerceiroBotao:1 })) */
+    this.setState((estadoAnterior, _props) => ({
+      cliquesTerceiroBotao: estadoAnterior.cliquesTerceiroBotao + 1
+    }))
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>Aperte Aqui!</button>
-        <button onClick={this.handleSecondClick}>Não aperte esse!</button>
-        <button onClick={this.handleThirdClick}>Não aperte aqui também!</button>
+        <button onClick={this.handleClick}>Aperte Aqui! {this.state.cliquesPrimeiroBotao}</button>
+        <button onClick={this.handleSecondClick}>Não aperte esse! {this.state.cliquesSegundoBotao}</button>
+        <button onClick={this.handleThirdClick}>Não aperte aqui também! {this.state.cliquesTerceiroBotao}</button>
       </div>
     );   
   }
