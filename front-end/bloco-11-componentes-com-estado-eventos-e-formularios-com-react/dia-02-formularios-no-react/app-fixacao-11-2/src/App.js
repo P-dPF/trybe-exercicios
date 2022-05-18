d988251578ebb6363c6254c54eb12bfae80a6de6
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-17 22:33:26 -0300

    fixação de componentes controlados sem textarea

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/app-fixacao-11-2/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/app-fixacao-11-2/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleTextInputChange = this.handleTextInputChange.bind(this);
+    this.handleNumberInputChange = this.handleNumberInputChange.bind(this);
+    /* this.handleTextareaChange = this.handleTextareaChange(this);
+ */
+    this.state = {
+      options: '',
+      textInput: '',
+      numberInput: 0,
+      textarea: '',
+    }
+  }
+
+  handleSelectChange(event) {
+    this.setState({
+      options: event.target.value,
+    });
+  };
+
+  handleTextInputChange(event) {
+    this.setState({
+      textInput: event.target.value,
+    })
+  };
+
+  handleNumberInputChange(event) {
+    this.setState({
+      numberInput: event.target.value,
+    })
+  };
+
+  /* handleTextareaChange(event) {
+    this.setState({
+      textarea: event.target.value,
+    })    
+  };
+ */
+  render() {
+
+    const { options, textInput, numberInput } = this.state;
+
+    return (
+      <>
+        <h1>Formulário Fixação Dia 11.2</h1>
+        <form>
+          <select value={options} onChange={this.handleSelectChange}>
+            <option>Opção A</option>
+            <option>Opção B</option>
+          </select>
+          <input type="text" value={textInput} onChange={this.handleTextInputChange}></input>
+          <input type="number" value={numberInput} onChange={this.handleNumberInputChange}></input>
+          {/* <textarea value={textarea} onChange={this.handleTextareaChange}></textarea> */}
+        </form>
+      </>
+    );
+  }
 }
 
 export default App;
