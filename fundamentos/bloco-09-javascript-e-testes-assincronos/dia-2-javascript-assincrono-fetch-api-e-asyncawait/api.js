fdd4c79d641aa0efddf8df92fcfc7309f0f6243e
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-19 23:59:43 -0300

    exercício 2 primeira parte

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -0,0 +1,24 @@
+const getCurrencies = async () => {
+  const url = `https://api.coincap.io/v2/assets`;
+  
+  const coins = await fetch(url)
+    .then(response => response.json())
+    .then(data => data.data)
+    .catch(error => error.toString());
+
+    return coins;
+}
+
+const showCoins = async () => {
+  const coins = await getCurrencies();
+  
+  const coinsList = document.getElementById('coins-list');
+  
+  coins.forEach((coin) => {
+    const listItem = document.createElement('li');
+    listItem.innerText = `${coin.name} (${coin.symbol}): ${coin.priceUsd}`;
+    coinsList.appendChild(listItem);
+  })
+}
+
+window.onload = () => showCoins();
