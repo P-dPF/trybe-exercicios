3491f3d992e2746dd5a1d35830625aef8b1774a1
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-23 21:13:13 -0300

    completa funções do app.js

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/lifecycleapp/src/App.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/lifecycleapp/src/App.js
@@ -6,7 +6,13 @@ import Connections from './components/Connections';
 
 class App extends React.Component {
   constructor() {
-    // 💡 me preencha!
+    super();
+
+    this.state = {
+      showProfile: true,
+    };
+
+    this.changeProfile = this.changeProfile.bind(this);
   }
 
   changeProfile() {
@@ -15,8 +21,35 @@ class App extends React.Component {
     this.setState({ showProfile: !showProfile });
   }
 
+  componentDidMount() {
+    const myUser = 'P-dPF'; //Preencha myUser com o seu user do GitHub.
+    try {
+      const url = `https://api.github.com/users/${myUser}`;
+      const response = await fetch(url)
+      const dataJson = await response.json()
+      this.setState({ api: dataJson })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render() {
-    // 💡 está faltando algo aqui!
+    const { showProfile } = this.state;
+    return (
+      <div className="gitNetwork d-flex flex-column justify-content-center">
+        {showProfile ? <Profile /> : null}
+        <div className="central d-flex justify-content-center">
+          <button
+            className="btn btn-dark align-self-center"
+            type="button"
+            onClick={this.changeProfile}
+          >
+            Mostrar / Ocultar Perfil
+          </button>
+        </div>
+        <Connections />
+      </div>
+    );
   }
 }
 
