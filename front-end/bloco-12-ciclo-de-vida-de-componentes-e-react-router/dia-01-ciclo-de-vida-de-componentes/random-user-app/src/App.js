4d604c2b3a8974a4f784d377b26ce935ed5f1cb5
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-24 21:34:35 -0300

    inicio fixação

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/random-user-app/src/App.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/random-user-app/src/App.js
@@ -6,13 +6,37 @@ class App extends React.Component {
     super();
 
     this.state = {
+      loading: true,
       randomUser: undefined,
     }
   }
 
+  componentDidMount() {
+    const url = 'https://api.randomuser.me/';
+    /* this.setState({ loading: true }) */
+    console.log('montou');
+
+   /*  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data.response)); */
+  }
+
+  /* buildProfile() {
+    const { randomUser } = this.state;
+    return (
+      <div>
+        <img src={ randomUser[0] } />
+      </div>
+    )
+  } */
+
   render() {
+    const { loading, randomUser } = this.state;
+    const loadingMessage = <div>Carregando...</div>
+
     return (
       <div className="App">
+        {/* {loading ? loadingMessage : } */}
       </div>
     );
   }
