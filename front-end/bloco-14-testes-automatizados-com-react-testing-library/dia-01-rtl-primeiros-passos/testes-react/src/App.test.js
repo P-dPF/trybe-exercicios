a2c1f18d09b4eb976664f029f2a13a103f7a877c
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-06-07 12:03:52 -0300

    inclui id para botão de envio e teste para este botão

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-01-rtl-primeiros-passos/testes-react/src/App.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-01-rtl-primeiros-passos/testes-react/src/App.test.js
@@ -14,8 +14,15 @@ test('Verificando se existe o campo Email.', () => {
 //   const btn = screen.getByRole('button');
 //   expect(btn).toBeInTheDocument();
 // });
-test('Verificando se existe dois botões', () => {
+test('Verificando se existem dois botões', () => {
   render(<App />);
   const buttons = screen.getAllByRole('button');
   expect(buttons).toHaveLength(2);
 })
+test('Verificando se existe um botão de enviar', () => {
+  render(<App />);
+  const btnSend = screen.getByTestId('id-send');
+  expect(btnSend).toBeInTheDocument();
+  expect(btnSend).toHaveProperty('type', 'button');
+  expect(btnSend).toHaveValue('Enviar');
+});
