be09daed7341125a45c18512c0763c30973e947a
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-06-07 12:01:34 -0300

    inclui mais um botão e modifica teste dos botões

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-01-rtl-primeiros-passos/testes-react/src/App.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-01-rtl-primeiros-passos/testes-react/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <input id="id-email" type="email" />
       </label>
       <input id="btn-send" type="button" value="Enviar" />
+      <input id="btn-back" type="button" value="Voltar" />
     </div>
   );
 }
