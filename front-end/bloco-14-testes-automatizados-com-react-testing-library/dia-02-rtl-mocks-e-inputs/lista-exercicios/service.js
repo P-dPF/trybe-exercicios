d89d2b0bcb3e1bc1b6f85cc27c931f708a7e42b2
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-06-08 11:53:50 -0300

    cria funções do exercicio 4

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/lista-exercicios/service.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/lista-exercicios/service.js
@@ -4,3 +4,21 @@ const generateNumber = () => {
 console.log(generateNumber());
 
 module.exports = { generateNumber };
+
+const upperCase = (string) => {
+  return string.toUpperCase();
+}
+
+console.log(upperCase('pedro'));
+
+const firstLetter = (string) => {
+  return string[0];
+}
+
+console.log(firstLetter('pedro'));
+
+const concatenate = (strOne, strTwo) => {
+  return strOne.concat(strTwo);
+}
+
+console.log(concatenate('pedro', 'pedro'));
