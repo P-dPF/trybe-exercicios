065e72f8aca4b4af6be94fb14acbd9d23803e509
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-06-08 11:47:52 -0300

    exercicio 3

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/lista-exercicios/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/lista-exercicios/service.test.js
@@ -12,3 +12,16 @@ test('testa nova implementação de generateNumber', () => {
   expect(service.generateNumber).toHaveBeenCalled();
   expect(service.generateNumber).toHaveBeenCalledTimes(1);
 });
+test('testa nova implementação de generateNumber', () => {
+  service.generateNumber = jest.fn().mockImplementation((a, b, c ) => a * b * c);
+  expect(service.generateNumber(2 , 5, 10)).toBe(100);
+  expect(service.generateNumber).toHaveBeenCalled();
+  expect(service.generateNumber).toHaveBeenCalledTimes(1);
+  expect(service.generateNumber).toHaveBeenCalledWith(2, 5, 10);
+
+  service.generateNumber.mockReset();
+  service.generateNumber = jest.fn().mockImplementation((a) => a * 2);
+  expect(service.generateNumber(5)).toBe(10);
+  expect(service.generateNumber).toHaveBeenCalledTimes(1);
+  expect(service.generateNumber).toHaveBeenCalledWith(5);
+});
