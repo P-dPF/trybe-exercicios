41f317fa619f4b4ce9e1062a2393aec909e39756
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-08-20 18:25:59 -0300

    instala e importa pacote readline, inclui perguntas do pacote para interação no terminal e modifica calculatebmi para exibir o bmi calculado  do usuario

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/my-scripts/bmi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/my-scripts/bmi.js
@@ -1,3 +1,11 @@
-const calculateBMI = (weight, height) => (
-  weight / (height ** 2)
-);
+const readline = require('readline-sync');
+
+const userWeight = readline.questionInt('What\'s your weight? ');
+const userHeight = readline.questionInt('What\'s your height? ');
+
+const calculateBMI = (weight, height) => {
+  const bmi = weight / (height ** 2)
+  console.log(bmi);
+}
+
+calculateBMI(userWeight, userHeight);
