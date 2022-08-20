67c77af2f99682d1d6d85eaa8ef9e87652d9807c
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-08-20 18:29:54 -0300

    substitui o metodo questionInt por questionFloat

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/my-scripts/bmi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/my-scripts/bmi.js
@@ -1,7 +1,7 @@
 const readline = require('readline-sync');
 
 const userWeight = readline.questionInt('What\'s your weight? ');
-const userHeight = readline.questionInt('What\'s your height? ');
+const userHeight = readline.questionFloat('What\'s your height? ');
 
 const calculateBMI = (weight, height) => {
   const bmi = weight / (height ** 2)
