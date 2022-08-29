fd95923b11f3f44cfa3550f4439e675c9d11ead9
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-08-29 16:29:21 -0300

    lista - exercicio 4 - cria endpoint para listar atividade por id

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-nodejs-api-rest-com-express/exercicios/3-a-6/app.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-nodejs-api-rest-com-express/exercicios/3-a-6/app.js
@@ -1,4 +1,37 @@
 const express = require('express');
 const app = express();
 
+const activities = [
+  {
+    id: 1,
+    description: 'Banho no cachorro',
+    status: 'A fazer',
+  },
+  {
+    id: 2,
+    description: 'Cortar a grama',
+    status: 'A fazer',
+  },
+  {
+    id: 3,
+    description: 'Estudar JavaScript',
+    status: 'Feito',
+  },
+];
+
+app.get('/myActivities/:id', (req, res) => {
+  const { id } = req.params;
+  let getActivityById;
+
+  for (let i = 0; i < activities.length; i +=1) {
+    const activity = activities[i];
+
+    if (activity.id === Number(id)) {
+      getActivityById = activity;
+    }
+  }
+
+  res.status(200).json({ getActivityById });
+});
+
 module.exports = app;
