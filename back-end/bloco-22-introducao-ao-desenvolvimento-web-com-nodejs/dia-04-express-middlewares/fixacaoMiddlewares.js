55c578b6b91d76565a1d23f6e785fe72093f5da6
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-08-30 14:33:38 -0300

    rearranja middleware de exemplo e inclui na chamada de put e post

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/fixacaoMiddlewares.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/fixacaoMiddlewares.js
@@ -10,15 +10,6 @@ const teams = [
 
 app.use(express.json());
 
-const validateTeam = (req, res, next) => {
-  const requiredProperties = ['nome', 'sigla'];
-  if (requiredProperties.every((property) => property in req.body)) {
-    next(); // Chama o próximo middleware
-  } else {
-    res.sendStatus(400); // Ou já responde avisando que deu errado
-  }
-};
-
 app.get('/teams', (req, res) => res.json(teams));
 
 app.get('/teams/:id', (req, res) => {
@@ -31,23 +22,27 @@ app.get('/teams/:id', (req, res) => {
   }
 });
 
-app.post('/teams', (req, res) => {
+const validateTeam = (req, res, next) => {
   const requiredProperties = ['nome', 'sigla'];
   if (requiredProperties.every((property) => property in req.body)) {
-    const team = { id: nextId, ...req.body };
-    teams.push(team);
-    nextId += 1;
-    res.status(201).json(team);
+    next(); // Chama o próximo middleware
   } else {
-    res.sendStatus(400);
+    res.sendStatus(400); // Ou já responde avisando que deu errado
   }
+};
+
+// Arranja os middlewares para chamar validateTeam primeiro
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
 });
 
-app.put('/teams/:id', (req, res) => {
+app.put('/teams/:id', validateTeam, (req, res) => {
   const id = Number(req.params.id);
-  const requiredProperties = ['nome', 'sigla'];
   const team = teams.find(t => t.id === id);
-  if (team && requiredProperties.every((property) => property in req.body)) {
+  if (team) {
     const index = teams.indexOf(team);
     const updated = { id, ...req.body };
     teams.splice(index, 1, updated);
