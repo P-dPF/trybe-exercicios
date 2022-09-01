321d4ee147dc11205be8e3a31fd067c35e18218e
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-09-01 15:11:18 -0300

    exercicio 7 - cria middlware para validar campo createdAt

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/exercicios/src/app.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/exercicios/src/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const validateName = require('./middlewares/validadeName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
+const validateCreatedAt = require('./middlewares/validateCreatedAt');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', validateName, validatePrice, validateDescription, (req, res) => {
+app.post('/activities', validateName, validatePrice, validateDescription, validateCreatedAt, (req, res) => {
   res.status(201).json({ message: "Atividade cadastrada com sucesso!" });
 });
 
