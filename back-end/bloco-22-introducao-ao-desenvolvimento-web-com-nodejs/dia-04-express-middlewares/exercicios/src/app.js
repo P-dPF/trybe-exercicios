7e2c72bd073aa75ae17a9f1f9e9eb3ee525e995f
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-09-01 14:24:07 -0300

    exercicio 5 - cria middleware para validar preço e o importa no app

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/exercicios/src/app.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/exercicios/src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const validateName = require('./middlewares/validadeName');
+const validatePrice = require('./middlewares/validatePrice');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', validateName, (req, res) => {
+app.post('/activities', validateName, validatePrice, (req, res) => {
   res.status(201).json({ message: "Atividade cadastrada com sucesso!" });
 });
 
