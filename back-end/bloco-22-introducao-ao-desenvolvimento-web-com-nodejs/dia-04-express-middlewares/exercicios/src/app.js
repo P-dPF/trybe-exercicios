80faf0ec5e1b732074c2bb90475104f518b4f66f
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-09-01 14:19:02 -0300

    exercicio 4 - cria middleware para validar nome e o importa no app

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/exercicios/src/app.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/exercicios/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const validateName = require('./middlewares/validadeName');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', (req, res) => {
+app.post('/activities', validateName, (req, res) => {
   res.status(201).json({ message: "Atividade cadastrada com sucesso!" });
 });
 
