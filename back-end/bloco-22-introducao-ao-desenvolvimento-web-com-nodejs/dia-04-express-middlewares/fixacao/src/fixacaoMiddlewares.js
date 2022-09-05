069af76b9805d8f11840379faa6cf3c13d7801fc
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-09-05 14:37:42 -0300

    commit final

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/fixacao/src/fixacaoMiddlewares.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const validateTeam = require('./middlewares/validateTeam');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const teamsRouter = require('./routes/teamsRouter');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(cors());
+
+let nextId = 3;
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
+
+app.use(express.json());
+
+app.use('/teams', teamsRouter);
+
+app.get('/teams', (req, res) => res.json(teams));
+
+const existingId = (req, res, next) => {
+  const { id } = req.params;
+  const hasId = teams.some((team) => team.id === Number(id));
+  return hasId
+    ? next()
+    : res.sendStatus(400);
+};
+
+app.get('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    res.json(team);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// Arranja os middlewares para chamar validateTeam primeiro
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
+});
+
+app.put('/teams/:id', validateTeam, existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    const index = teams.indexOf(team);
+    const updated = { id, ...req.body };
+    teams.splice(index, 1, updated);
+    res.status(201).json(updated);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.delete('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    const index = teams.indexOf(team);
+    teams.splice(index, 1);
+  }
+  res.sendStatus(200);
+});
+
+app.use((req, res) => res.sendStatus(404));
+
+module.exports = app;
---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-middlewares/fixacao/src/fixacaoMiddlewaresServer.js
@@ -0,0 +1,3 @@
+const app = require('./fixacaoMiddlewares');
+
+app.listen(3002, () => console.log('Server running on port 3002'));
