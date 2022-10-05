b97c5c44b883dee944585b179222fa01128f930f
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-10-05 17:27:57 -0300

    fixação: type aliases

---
+++ back-end/secao-8-introducao-a-typescript/dia-2-tipagem-estatica-e-generics/fixacao/fixacao.ts
@@ -0,0 +1,13 @@
+type Bird = {
+  color: string;
+  ageSpan: number;
+};
+
+type Sum = (firstNum: number, secondNum: number) => number;
+
+type Address = {
+  street: string;
+  number: number;
+  city: string;
+  country: string;
+};
