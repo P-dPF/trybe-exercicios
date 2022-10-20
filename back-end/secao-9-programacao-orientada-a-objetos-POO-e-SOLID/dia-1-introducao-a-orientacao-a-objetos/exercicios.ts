b097215870f0cbf29f1eeaab2efdf54357221420
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-10-19 21:21:46 -0300

    exercicio 2

--- back-end/secao-9-programacao-orientada-a-objetos-POO-e-SOLID/dia-1-introducao-a-orientacao-a-objetos/exercicios.ts
+++ back-end/secao-9-programacao-orientada-a-objetos-POO-e-SOLID/dia-1-introducao-a-orientacao-a-objetos/exercicios.ts
@@ -20,4 +20,17 @@ class EnrolledStudents {
   get name() { return this.name; }
   get examsGrades() { return this.examsGrades; }
   get assignmentsGrades() { return this.assignmentsGrades; }
+
+  sumGrades = (): number => {
+    const examsGrades = this._examsGrades.reduce((acc, curr) => acc + curr);
+    const assignmentsGrades = this._assignmentsGrades.reduce((acc, curr) => acc + curr);
+
+    return examsGrades + assignmentsGrades;
+  }
+
+  averageOfGrades = (): number => {
+    const sum = this.sumGrades()
+
+    return sum / 6;
+  }
 }
