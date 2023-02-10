6b4acd9acec13ac46679c78b733f713c73281170
Pedro Franca <pedrodepaulafranca@gmail.com>
2023-02-09 23:36:44 -0300

    acrescenta caso de teste fizzbuzz e adiciona esta funcionalidade à função

--- ciencia-da-computacao/dia-1.3-testes/test_funcoes.py
+++ ciencia-da-computacao/dia-1.3-testes/test_funcoes.py
@@ -2,7 +2,9 @@ def fizz_buzz(numbers_list):
     fizz_buzz_list = []
 
     for number in numbers_list:
-        if number % 3 == 0:
+        if number % 3 == 0 and number % 5 == 0:
+            fizz_buzz_list.append("FizzBuzz")
+        elif number % 3 == 0:
             fizz_buzz_list.append("Fizz")
         elif number % 5 == 0:
             fizz_buzz_list.append("Buzz")
@@ -15,3 +17,4 @@ def fizz_buzz(numbers_list):
 def test_fizz_buzz():
     assert fizz_buzz([1, 2, 3]) == [1, 2, "Fizz"]
     assert fizz_buzz([1, 5, 2]) == [1, "Buzz", 2]
+    assert fizz_buzz([15, 7, 2]) == ["FizzBuzz", 7, 2]
