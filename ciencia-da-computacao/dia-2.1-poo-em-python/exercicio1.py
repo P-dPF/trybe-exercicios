263e7e9dbca91fe7acab4d21ef631dd5241678c3
Pedro Franca <pedrodepaulafranca@gmail.com>
2023-02-10 23:12:35 -0300

    EXERCICIO 1: declara métodos da classe TV

--- ciencia-da-computacao/dia-2.1-poo-em-python/exercicio1.py
+++ ciencia-da-computacao/dia-2.1-poo-em-python/exercicio1.py
@@ -4,3 +4,19 @@ class TV:
         self.__canal = 1
         self.__tamanho = tamanho
         self.__ligada = False
+
+    def aumentar_volume(self):
+        while self.__volume <= 99:
+            self.__volume += 1
+
+    def diminuir_volume(self):
+        while self.__volume >= 0:
+            self.__volume -= 1
+
+    def modificar_canal(self, canal):
+        if 1 <= canal <= 99:
+            self.__canal = canal
+        raise ValueError("Canal inexistente")
+
+    def ligar_desligar(self):
+        self.__ligada = not self.__ligada
