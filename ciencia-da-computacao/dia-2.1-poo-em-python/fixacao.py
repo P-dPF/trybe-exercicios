ad12f017d6bfc4b57fa5bb6a8f25e4a9f3ed7a36
Pedro Franca <pedrodepaulafranca@gmail.com>
2023-02-10 21:38:47 -0300

    fixação: instacia classes, chama métodos e printa resultados

--- ciencia-da-computacao/dia-2.1-poo-em-python/fixacao.py
+++ ciencia-da-computacao/dia-2.1-poo-em-python/fixacao.py
@@ -16,7 +16,22 @@ class Pessoa:
             self.saldo_na_conta -= ventilador.preco
             self.ventilador = ventilador
 
+    def __str__(self):
+        if self.ventilador:
+            return f"{self.nome} possui o ventilador {self.ventilador.nome}"
+        return f"{self.nome} não possui o ventilador"
+
 
 class Ventilador:
-    def __init__(self, preco):
+    def __init__(self, nome, preco):
+        self.nome = nome
         self.preco = preco
+
+
+ventilador_de_teto = Ventilador("Arno", 50)
+
+pessoa_exemplo = Pessoa("Pedro", 150)
+
+pessoa_exemplo.comprar_ventilador(ventilador_de_teto)
+
+print(pessoa_exemplo)
