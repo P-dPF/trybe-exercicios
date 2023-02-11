d4fdee3be43281260822746707224d21c95e60d0
Pedro Franca <pedrodepaulafranca@gmail.com>
2023-02-10 21:27:49 -0300

    fixação: copia classe Pessoa e implementa classe Ventilador

---
+++ ciencia-da-computacao/dia-2.1-poo-em-python/fixacao.py
@@ -0,0 +1,16 @@
+# EXERCÍCIO 1
+class Pessoa:
+    def __init__(self, nome, saldo_na_conta):
+        self.nome = nome
+        self.saldo_na_conta = saldo_na_conta
+        self.liquidificador = None
+
+    def comprar_liquidificador(self, liquidificador):
+        if liquidificador.preco <= self.saldo_na_conta:
+            self.saldo_na_conta -= liquidificador.preco
+            self.liquidificador = liquidificador
+
+
+class Ventilador:
+    def __init__(self, preco):
+        self.preco = preco
